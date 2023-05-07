8de3c685ff9bd80075b6cbaf594e23d4feec1c73
Francis <francisbernal14@gmail.com>
2023-05-07 23:45:09 +0200

    feat: create <plates> object data

---
+++ app/data/plates.js
@@ -0,0 +1,250 @@
+export const plates = {
+    salads: [
+        {
+            name: "Beicon",
+            price: "8.50",
+            descripcion: "Mix de lechugas, medallón de beicon y queso de cabra, frutos secos y salsa de miel y mostaza.",
+            img: "",
+            alt: "ensalada de bacon y queso de cabra",
+            allergens: ["cascara", "mostaza", "sesamo", "lacteo", "sulfito"]
+        },
+
+        {
+            name: "Cesar",
+            price: "8.50",
+            descripcion: "Mix de lechugas, pollo, picatostes y escamas de parmesano.",
+            img: "",
+            alt: "ensalada cesar",
+            allergens: ["gluten", "lacteo", "sulfito"]
+        }
+    ],
+
+    snacks: [
+        {
+            name: "Twister de langostinos",
+            price: "8",
+            descripcion: "6 Uds. con salsa sweet chili.",
+            img: "",
+            alt: "twister de langostinos",
+            allergens: ["soja", "crustaceo", "pescado", "gluten"]
+        },
+
+        {
+            name: "Lagrimitas de pollo",
+            price: "8.50",
+            descripcion: "Con patatas fritas y salsa gaucha argentina.",
+            img: "",
+            alt: "lagrimitas de pollo",
+            allergens: ["gluten", "huevo", "mostaza"]
+        },
+
+        {
+            name: "Bravas a la Camboria",
+            price: "5.50",
+            descripcion: "Papas fritas con salsa brava al estilo de Madrid.",
+            img: "",
+            alt: "papas bravas madrileñas",
+            allergens: ["sesamo"]
+        },
+
+        {
+            name: "Roscas",
+            price: "7.50",
+            descripcion: "De jamón serrano ó de lomo, con tomate y queso.",
+            img: "",
+            alt: "rosca de jamon o lomo de cerdo",
+            allergens: ["gluten", "lacteo"]
+        },
+
+        {
+            name: "Camenbert frito",
+            price: "8",
+            descripcion: "3 uds. con coulis de arándanos.",
+            img: "",
+            alt: "queso frito camembert",
+            allergens: ["gluten", "lacteo", "huevo"]
+        },
+
+        {
+            name: "Risotto",
+            price: "9.50",
+            descripcion: "De champiñones, solomillo y parmesano.",
+            img: "",
+            alt: "risotto de solomillo",
+            allergens: ["lacteo", "soja"]
+        },
+
+        {
+            name: "Cazuela de papas gratinadas",
+            price: "7.50",
+            descripcion: "Papas fritas con mayonesa, beicon y 3 quesos gratinados.",
+            img: "",
+            alt: "cazuela de papas gratinadas con bacon",
+            allergens: ["lacteo", "huevo"]
+        },
+    ],
+
+    dishes: [
+        {
+            name: "Croquetas de la casa",
+            price: "9 / 4.50",
+            descripcion: "8 / 4 uds. de puchero, rabo de toro ó pollo al curry.",
+            img: "",
+            alt: "croquetas caseras de la casa",
+            allergens: ["lacteo", "huevo", "gluten", "soja"]
+        },
+
+        {
+            name: "Flamenquín Cordobés",
+            price: "9",
+            descripcion: "Relleno de lomo, jamón serrano, queso y pimiento.",
+            img: "",
+            alt: "flamenquin cordobes casero",
+            allergens: ["lacteo", "huevo", "gluten"]
+        },
+
+        {
+            name: "Pollo teriyaki",
+            price: "10",
+            descripcion: "Con arroz y salsa sweet chili.",
+            img: "",
+            alt: "pollo teriyaki con arroz",
+            allergens: ["lacteo", "huevo", "gluten"]
+        },
+
+        {
+            name: "Cazuela de huevos rotos y virutas de foie",
+            price: "9",
+            descripcion: "Papas panaderas con virutas de jamón serrano y foie.",
+            img: "",
+            alt: "huevos rotos con jamon y foie",
+            allergens: ["lacteo", "huevo"]
+        },
+
+        {
+            name: "Pan Bao de solomillo ibérico",
+            price: "10",
+            descripcion: "Con mayokimchi (toque agripicante).",
+            img: "",
+            alt: "pan bao de solomillo",
+            allergens: ["gluten", "huevo"]
+        },
+
+        {
+            name: "Tostas",
+            price: "8",
+            descripcion: {
+                1: "Queso de cabra, cebolla caramelizada y miel de caña.",
+                2: "Gratinado de alioli, parmesano y tres quesos."
+            },
+            img: "",
+            alt: "tosta de pan de ajo o queso de cabra",
+            allergens: ["gluten", "lacteo"]
+        },
+
+        {
+            name: "Wok de noodles Udon",
+            price: "10",
+            descripcion: "Pídelos de pollo, gambas ó mixto (+1€)",
+            img: "",
+            alt: "wok de noodles udon con pollo y gambas",
+            allergens: ["gluten", "cascara", "sesamo", "soja", "crustaceo", "pescado"]
+        }
+    ],
+
+    fishes: [
+        {
+            name: "Cazón en adobo frito",
+            price: "8.50",
+            descripcion: null,
+            img: "",
+            alt: "cazon frito en adobo",
+            allergens: ["gluten", "pescado", "sulfito"]
+        },
+
+        {
+            name: "Almejas a la marinera",
+            price: "9.50",
+            descripcion: null,
+            img: "",
+            alt: "almejas a la marinera",
+            allergens: ["crustaceo", "sulfito"]
+        },
+
+        {
+            name: "Pata de pulto",
+            price: "14",
+            descripcion: "A la plancha, sobre cremoso de romesco y alioli de pimentón de La Vera.",
+            img: "",
+            alt: "pata de pulpo a la plancha con romesco",
+            allergens: ["cascara", "sesamo", "sulfito", "pescado"]
+        }
+    ], 
+
+    demanding: [
+        {
+            name: "Pluma ibérica BT",
+            price: "14",
+            descripcion: "Cocinada a baja temperatura, con jamón serrano y aceite trufado.",
+            img: "",
+            alt: "pluma iberica a baja temperatura con patatas",
+            allergens: null
+        },
+
+        {
+            name: "Costillas de cerdo BBQ",
+            price: "14",
+            descripcion: "Cocinadas a baja temperatura con salsa bbq y papas.",
+            img: "",
+            alt: "costillas de cerdo barbacoa",
+            allergens: ["sesamo"]
+        },
+
+        {
+            name: "Milhojas",
+            price: "9",
+            descripcion: "Con queso de cabra, jamón serrano y confitura de mango.",
+            img: "",
+            alt: "milhojas de queso cabra, mango y jamon",
+            allergens: ["gluten", "lacteo"]
+        },
+
+        {
+            name: "Sacos de calabacín",
+            price: "8",
+            descripcion: "Rellenos de bechamel de puerros y boletus.",
+            img: "",
+            alt: "sacos de calabacin rellenos",
+            allergens: ["huevo", "lacteo"]
+        },
+
+        {
+            name: "Churrasco de pollo",
+            price: "9",
+            descripcion: "Con chutney de mango y quinoa con verduras.",
+            img: "",
+            alt: "churrasco de pollo con quinoa",
+            allergens: null
+        }
+    ],
+
+    burguers: [
+        {
+            name: "Ternera y buey (200gr)",
+            price: "10",
+            descripcion: "Queso de cabra, cebolla caramelizada, acompañada de papas.",
+            img: "",
+            alt: "pata de pulpo a la plancha con romesco",
+            allergens: ["gluten", "sesamo", "lacteo"]
+        },
+
+        {
+            name: "Angus (200gr)",
+            price: "10",
+            descripcion: "Doble de beicon, queso cheddar, mix de lechugas y cebolla crunchy.",
+            img: "",
+            alt: "pata de pulpo a la plancha con romesco",
+            allergens: ["gluten", "sesamo", "lacteo", "mostaza"]
+        }
+    ]
+}
