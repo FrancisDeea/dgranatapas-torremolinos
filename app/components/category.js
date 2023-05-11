c42dc899c763119039d7eb3087b82f45e652ca37
Francis <francisbernal14@gmail.com>
2023-05-11 23:21:39 +0200

    fix: some little issues

--- app/components/category.js
+++ app/components/category.js
@@ -1,17 +1,21 @@
+'use client'
+
 import styles from './category.module.scss';
 
-import Image from 'next/image'
+import Image from 'next/image';
+import {useRouter} from 'next/navigation';
 
-export default function Category({ title, url }) {
+export default function Category({ title, img, url }) {
+    const router = useRouter();
 
     return (
-        <div className={styles.container}>
-            <Image
-                src={url}
-                className={`${styles.img}`}
-                fill
-            />
-            <h2 className={styles.title}>{title}</h2>
-        </div>
+        <div className={styles.container} onClick={() => router.push(url)}>
+                <Image
+                    src={img}
+                    className={`${styles.img}`}
+                    fill
+                />
+                <h2 className={styles.title}>{title}</h2>
+¡        </div>
     )
 }
--- app/menu/page.js
+++ app/menu/page.js
@@ -15,12 +15,12 @@ export default function Menu() {
             </Head>
             <seccion className="page_container">
                 <div className={styles.category_container}>
-                    <Category title="Ensaladas" url="/assets/platos/ensalada-bacon-queso-cabra.webp" />
-                    <Category title="Entrantes" url="/assets/platos/patatas-bravas-caseras.webp" />
-                    <Category title="Platos" url="/assets/platos/flamenquin-cordobes-casero.webp" />
-                    <Category title="Pescados" url="/assets/platos/pata-pulpo-salsa-romesco.webp" />
-                    <Category title="Exigentes" url="/assets/platos/milhojas-queso-cabra-mango-jamon.webp" />
-                    <Category title="Hamburguesas" url="/assets/platos/hamburguesa-ternera-buey.webp" />
+                    <Category title="Ensaladas" img="/assets/platos/ensalada-bacon-queso-cabra.webp" url="/menu/ensaladas" />
+                    <Category title="Entrantes" img="/assets/platos/patatas-bravas-caseras.webp" url="/menu/entrantes" />
+                    <Category title="Platos" img="/assets/platos/flamenquin-cordobes-casero.webp" url="/menu/platos" />
+                    <Category title="Pescados" img="/assets/platos/pata-pulpo-salsa-romesco.webp" url="/menu/pescados" />
+                    <Category title="Exigentes" img="/assets/platos/milhojas-queso-cabra-mango-jamon.webp" url="/menu/platos-exigentes" />
+                    <Category title="Hamburguesas" img="/assets/platos/hamburguesa-ternera-buey.webp" url="/menu/hamburguesas-buey-angus" />
                 </div>
             </seccion>
         </>
