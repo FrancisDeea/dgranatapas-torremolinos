e5ace0caec3c689855b7b7c5e7cdf7697e2b5a1a
Francis <francisbernal14@gmail.com>
2023-06-13 20:39:59 +0200

    fix: images of categories in menu page and some style issues

--- app/components/category.js
+++ app/components/category.js
@@ -5,15 +5,18 @@ import styles from './category.module.scss';
 import Image from 'next/image';
 import {useRouter} from 'next/navigation';
 
-export default function Category({ title, img, url }) {
+export default function Category({ title, img, url, alt }) {
     const router = useRouter();
 
     return (
         <div className={styles.container} onClick={() => router.push(url)}>
                 <Image
+                    alt={alt}
                     src={img}
                     className={`${styles.img}`}
                     fill
+                    sizes="(max-width: 1200px) 50vw, 33vw"
+
                 />
                 <h2 className={styles.title}>{title}</h2>
 ¡        </div>
--- app/menu/page.js
+++ app/menu/page.js
@@ -25,15 +25,15 @@ export default function Menu() {
             </Head>
             <seccion className="page_container">
                 <div className={styles.category_container}>
-                    <Category title="Ensaladas" img="/assets/platos/ensalada-bacon-queso-cabra.webp" url="/menu/ensaladas" />
-                    <Category title="Entrantes" img="/assets/platos/patatas-bravas-caseras.webp" url="/menu/entrantes" />
-                    <Category title="Platos" img="/assets/platos/flamenquin-cordobes-casero.webp" url="/menu/platos" />
-                    <Category title="Pescados" img="/assets/platos/pata-pulpo-salsa-romesco.webp" url="/menu/pescados" />
-                    <Category title="Exigentes" img="/assets/platos/milhojas-queso-cabra-mango-jamon.webp" url="/menu/platos-exigentes" />
-                    <Category title="Hamburguesas" img="/assets/platos/hamburguesa-ternera-buey.webp" url="/menu/hamburguesas-buey-angus" />
-                    <Category title="Asiático" img="/assets/platos/pan-bao-solomillo-2.webp" url="/menu/platos-asiaticos" />
-                    <Category title="Postres" img="/assets/platos/tarta de queso viña.webp" url="/menu/postres" />
-                    <Category title="Bebidas" img="" url="/menu/bebidas" />
+                    <Category title="Ensaladas" img="/assets/platos/ensalada-bacon-queso-cabra.webp" alt="ensaladas" url="/menu/ensaladas" />
+                    <Category title="Entrantes" img="/assets/platos/patatas-bravas-caseras.webp" alt="entrantes" url="/menu/entrantes" />
+                    <Category title="Platos" img="/assets/platos/flamenquin-cordobes-casero.webp" alt="platos principales" url="/menu/platos" />
+                    <Category title="Pescados" img="/assets/platos/pata-pulpo-salsa-romesco.webp" alt="pescados frescos" url="/menu/pescados" />
+                    <Category title="Exigentes" img="/assets/platos/milhojas-queso-cabra-mango-jamon.webp" alt="platos exigentes" url="/menu/platos-exigentes" />
+                    <Category title="Hamburguesas" img="/assets/platos/hamburguesa-ternera-buey.webp" alt="hamburguesas de angus y buey" url="/menu/hamburguesas-buey-angus" />
+                    <Category title="Asiático" img="/assets/platos/pan-bao-solomillo-2.webp" alt="platos asiáticos" url="/menu/platos-asiaticos" />
+                    <Category title="Postres" img="/assets/platos/tarta de queso viña.webp" alt="postres" url="/menu/postres" />
+                    <Category title="Bebidas" img="/assets/platos/bebidas-dgrana.webp" alt="bebidas" url="/menu/bebidas" />
                 </div>
             </seccion>
         </>
